50406a7c627b184981720a2975d0bad71e1251d4
Łukasz Biel <lukasz.p.biel@gmail.com>
2022-01-23 22:52:37 +0100

    WIP broken state, so that pbr pipeline knowledge is kept

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use crate::grid::EndlessGridPlugin;
+
+mod grid;
 
 #[derive(Component)]
 struct MainCamera;
@@ -13,6 +16,7 @@ fn main() {
             brightness: 0.5,
         })
         .add_plugins(DefaultPlugins)
+        // .add_plugin(EndlessGridPlugin)
         .add_startup_system(setup)
         .add_system_to_stage(CoreStage::Update, auto_fly_ship)
         .add_system_to_stage(CoreStage::Update, camera_follow_spaceship)
@@ -24,18 +28,40 @@ fn setup(
     mut scene_spawner: ResMut<SceneSpawner>,
     asset_server: Res<AssetServer>,
 ) {
-    let player_ship_entity = commands.spawn().insert(Transform::default()).insert(GlobalTransform::default()).insert(PlayerShip).id();
+    let player_ship_entity = commands
+        .spawn()
+        .insert(Transform::default())
+        .insert(GlobalTransform::default())
+        .insert(PlayerShip)
+        .id();
     scene_spawner.spawn_as_child(
         asset_server.load("models/spaceship.gltf#Scene0"),
         player_ship_entity,
     );
-    commands.spawn_scene(asset_server.load("models/quad.gltf#Scene0"));
     commands
         .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(0.0, 20.0, -35.0).looking_at(Vec3::new(0.0, 5.0, 0.0), Vec3::Y),
+            transform: Transform::from_xyz(0.0, 20.0, -35.0)
+                .looking_at(Vec3::new(0.0, 5.0, 0.0), Vec3::Y),
             ..Default::default()
         })
         .insert(MainCamera);
+}
+
+fn setup_quad(mut commands: Commands, asset_server: Res<AssetServer>, mut scenes: ResMut<Assets<Scene>>) {
+    let handle = asset_server.load("models/quad.gltf#Mesh0");
+    commands.spawn_bundle(PbrBundle {
+        mesh: handle,
+        material: (),
+        transform: Default::default(),
+        global_transform: Default::default(),
+        visibility: Default::default(),
+        computed_visibility: Default::default()
+    })
+    commands.spawn_scene(handle);
+}
+
+fn setup_quad2(mut commands: Commands, mut scenes: ResMut<Assets<Scene>>) {
+    scenes.get_mut()
 }
 
 fn camera_follow_spaceship(
@@ -60,11 +86,16 @@ fn camera_follow_spaceship(
     camera.look_at(ship_object.translation + Vec3::Y * 5.0, Vec3::Y);
 }
 
-fn auto_fly_ship(q_spaceship: Query<&Children, With<PlayerShip>>, mut q_transforms: Query<&mut Transform>) {
+fn auto_fly_ship(
+    q_spaceship: Query<&Children, With<PlayerShip>>,
+    mut q_transforms: Query<&mut Transform>,
+) {
     let mut ship = if let Ok(children) = q_spaceship.get_single() {
         let child = children.first().unwrap();
         q_transforms.get_mut(*child).unwrap()
-    } else { return };
+    } else {
+        return;
+    };
 
     ship.translation += Vec3::new(0.0, 0.0, 0.1);
 }
